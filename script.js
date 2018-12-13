1dc4332a9dc7b8deb36e99087fa795381bc6c937
JohnA <jaboyak@gmail.com>
2018-12-13 09:19:52 -0700

    updated

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 document.addEventListener('DOMContentLoaded', function() {
+    
+    // create an array of todos
+    var todosArray = []
+
     // target the form
     var theForm = document.getElementById('the-form')
 
@@ -15,15 +19,30 @@ document.addEventListener('DOMContentLoaded', function() {
         // store the value of the input field
         var newTodo = newTodoField.value
 
+        // add the new todo to the todos array
+        todosArray.push(newTodo)
+
+        // clear out the existing todos
+        document.getElementById('list-of-todos').innerHTML = ""
+        
+        // loop over the todos array and create an list item for each todo
+        // and append each list item to the DOM
+        for(var i = 0; i < todosArray.length; i++) {
+            createListItem(todosArray[i])
+        }
+        
+    })
+
+    function createListItem(todoText) {
         // create a new list item element
         var newListItem = document.createElement('li')
 
         // add the todo to the list item text
-        newListItem.innerText = newTodo
+        newListItem.innerText = todoText
 
         console.log('the new list item', newListItem)
         var listOfTodos = document.getElementById('list-of-todos')
 
         listOfTodos.appendChild(newListItem)
-    })
+    }
 })
